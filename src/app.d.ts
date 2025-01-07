9cb90a7a685ce6a747965f5e32d349b87c28d3b9
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-01-07 00:41:29 +0000

    Implemented basic user class and added into PageData interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,14 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import { User } from "$lib/User";
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			user: User
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
