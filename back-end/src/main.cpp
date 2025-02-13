b69c20a007c4097a41b22148d69468106cd8ce5e
Nathan Baines <nb535@sussex.ac.uk>
2025-02-13 16:28:56 +0000

    Testing running from back-end exe

--- back-end/src/main.cpp
+++ back-end/src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "----- CodeCat -----" << std::endl 
+        << "CodeCat © Nathcat 2025" << std::endl << std::endl
+        << "Starting front end..." << std::endl;
+
+    std::cout << "npx tsc";
 
     return 0;
 }
